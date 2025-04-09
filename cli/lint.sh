3a53cd9c0a32afe11e966ccfbfc43ad20127a6e2
Kenneth VanderLinde <kwvanderlinde@gmail.com>
2025-04-09 15:45:17 -0700

    Simplify metainfo file name

--- cli/lint.sh
+++ cli/lint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 printf "Linting metainfo\n"
-flatpak run --command=flatpak-builder-lint org.flatpak.Builder appstream com.kwvanderlinde.MapTool.metainfo.xml
+flatpak run --command=flatpak-builder-lint org.flatpak.Builder appstream metainfo.xml
 printf "Linting manifest\n"
 flatpak run --command=flatpak-builder-lint org.flatpak.Builder manifest com.kwvanderlinde.MapTool.yml
 printf "Linting repo\n"
