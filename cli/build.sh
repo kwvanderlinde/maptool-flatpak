e23487b94a16717b1c2bff9e3b8e2e3e0b6adbc6
Kenneth VanderLinde <kwvanderlinde@gmail.com>
2025-04-08 22:09:56 -0700

    Use org.flatpak.Builder so user does not need to install flatpak-builder

--- cli/build.sh
+++ cli/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-flatpak-builder --force-clean --user --install-deps-from=flathub --repo=repo --install builddir com.kwvanderlinde.MapTool.yml
+flatpak remote-add --if-not-exists --user flathub https://dl.flathub.org/repo/flathub.flatpakrep
+flatpak install --user -y flathub org.flatpak.Builder
+flatpak run org.flatpak.Builder --force-clean --sandbox --user --install --install-deps-from=flathub --ccache --mirror-screenshots-url=https://dl.flathub.org/media/ --repo=repo builddir com.kwvanderlinde.MapTool.yml
