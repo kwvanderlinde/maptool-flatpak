d619298d39af40ec141e04fb3be3b15df7c7836e
Kenneth VanderLinde <kwvanderlinde@gmail.com>
2025-05-21 14:53:11 -0700

    Add README.md and scripts under cli/ to make things easy

---
+++ cli/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+flatpak remote-add --if-not-exists --user flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak install --user -y flathub org.flatpak.Builder
+flatpak run org.flatpak.Builder --force-clean --sandbox --user --install --install-deps-from=flathub --ccache --mirror-screenshots-url=https://dl.flathub.org/media/ --repo=repo builddir com.kwvanderlinde.MapTool.yml
